0aa7155459ae9ce7bb6dfb9c749c1d8193e83f8e
EC2 Default User <ec2-user@ip-172-31-37-42.ap-southeast-1.compute.internal>
2019-03-08 20:39:45 +0000

    sidebar

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,10 @@
 class ContactsController < ApplicationController
   def index
-    @contacts = Contact.page(params[:page])
+    # 該当するグループIDだけ取得
+    if params[:group_id] && !params[:group_id].empty?
+      @contacts = Contact.where(group_id: params[:group_id]).page(params[:page])
+    else
+      @contacts = Contact.page(params[:page])
+    end
   end
 end
