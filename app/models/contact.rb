65af994da53ef48a0d04df9674bc1f0a5da18e2e
EC2 Default User <ec2-user@ip-172-31-37-42.ap-southeast-1.compute.internal>
2019-03-08 19:35:44 +0000

    gravarar

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,9 @@
 class Contact < ApplicationRecord
-  belongs_to :group
+    belongs_to :group
+  
+    def gravatar
+        hash = Digest::MD5.hexdigest(email.downcase)
+        # compile URL which can be used in <img src="RIGHT_HERE"...
+        "https://www.gravatar.com/avatar/#{hash}"
+    end
 end
